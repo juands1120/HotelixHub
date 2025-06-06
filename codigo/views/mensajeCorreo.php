1b39b18adbf68d40cefa1b7a62b11ca22d923466
juands1120 <juandis.pt55@gmail.com>
2025-06-05 23:12:41 -0500

    dash mensaje de correo

---
+++ codigo/views/mensajeCorreo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Enlace enviado</title>
+</head>
+<body>
+    <h1>¡Verifica tu correo!</h1>
+    <p><?php echo htmlspecialchars($message); ?></p>
+</body>
+</html>
