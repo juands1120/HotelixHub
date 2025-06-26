c9dd3b5f29d62183087542138574eaf0aaa23a18
Sebassss23 <sr080008@gmail.com>
2025-06-25 19:16:42 -0500

    codigo bdd

--- BDD/HotelixHub.sql
+++ BDD/HotelixHub.sql
@@ -4,7 +4,8 @@ CREATE TABLE `hotelixhub`.`rol` (
 );
 
 
-CREATE TABLE `hotelixhub`.`usuarios` (
+-- Tabla: Usuario
+CREATE TABLE IF NOT EXISTS usuarios (
   id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   usu_idrol INT NOT NULL,
   nombre VARCHAR(100) NOT NULL,
@@ -17,6 +18,8 @@ CREATE TABLE `hotelixhub`.`usuarios` (
   password VARCHAR(255) NOT NULL,
   reset_token VARCHAR(255) DEFAULT NULL,
   token_expires DATETIME DEFAULT NULL,
+  estado ENUM("en turno", "fuera de turno", "vacaciones")
+  direccion varchar (100) NOT NULL,
   FOREIGN KEY (usu_idrol) REFERENCES rol(id_rol)
 );
 
@@ -45,7 +48,7 @@ CREATE TABLE `hotelixhub`.`reserva` (
     precio_total DECIMAL(10,2) NOT NULL,
     estado ENUM('Pendiente', 'Confirmada', 'Cancelada') DEFAULT 'Pendiente',
     fecha_reserva TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE SET NULL,
     FOREIGN KEY (id_habitacion) REFERENCES habitacion(id_habitacion)
 );
 
@@ -61,3 +64,76 @@ CREATE TABLE contacto (
     id_usuario INT DEFAULT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario) ON DELETE SET NULL
 );
+
+
+CREATE TABLE categoria (
+  id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL UNIQUE,
+  descripcion varchar (100) not null
+);
+
+
+CREATE TABLE producto (
+  id_producto INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL,
+  descripcion varchar (100) not null,
+  precio_unitario DECIMAL(10,2) NOT NULL,
+  stock INT NOT NULL,
+  imagen VARCHAR(255),
+  id_categoria INT,
+  FOREIGN KEY (id_categoria) REFERENCES categoria (id_categoria)
+);
+
+CREATE TABLE venta (
+  id_venta INT AUTO_INCREMENT PRIMARY KEY,
+  id_usuario INT NOT NULL,
+  fecha_venta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  fecha_anulacion TIMESTAMP NULL,
+  total DECIMAL(10,2) NOT NULL,
+  estado ENUM('Activa', 'Anulada') DEFAULT 'Activa',
+  FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
+);
+
+CREATE TABLE detalle_venta (
+  id_detalle INT AUTO_INCREMENT PRIMARY KEY,
+  id_venta INT NOT NULL,
+  id_producto INT NOT NULL,
+  cantidad INT NOT NULL,
+  precio_unitario DECIMAL(10,2) NOT NULL,
+  subtotal DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (id_venta) REFERENCES venta(id_venta),
+  FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
+);
+
+
+
+
+
+/* codigo de ejemplo para bdd orientada a objetos*/
+
+CREATE TABLE fecha_evento (
+  id_fecha INT AUTO_INCREMENT PRIMARY KEY,
+  fecha DATE NOT NULL,
+  tipo_fecha ENUM('reserva', 'entrada', 'salida', 'venta', 'contacto') NOT NULL,
+  descripcion TEXT
+);
+
+
+CREATE TABLE reserva (
+  id_reserva INT AUTO_INCREMENT PRIMARY KEY,
+  id_usuario INT NOT NULL,
+  id_habitacion INT NOT NULL,
+  id_fecha_entrada INT,
+  id_fecha_salida INT,
+  id_fecha_reserva INT,
+  num_huespedes INT NOT NULL,
+  servicios_adicionales TEXT,
+  precio_total DECIMAL(10,2) NOT NULL,
+  estado ENUM('Pendiente', 'Confirmada', 'Cancelada') DEFAULT 'Pendiente',
+  FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+  FOREIGN KEY (id_habitacion) REFERENCES habitacion(id_habitacion),
+  FOREIGN KEY (id_fecha_entrada) REFERENCES fecha_evento(id_fecha),
+  FOREIGN KEY (id_fecha_salida) REFERENCES fecha_evento(id_fecha),
+  FOREIGN KEY (id_fecha_reserva) REFERENCES fecha_evento(id_fecha)
+);
+
